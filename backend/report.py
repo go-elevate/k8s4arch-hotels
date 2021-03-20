087a4fe8e1d67c9c5d06209dbb8d184b2a407ae2
Federico Catinello <federico@ingenia.la>
2021-03-19 23:39:35 -0300

    add daily report

---
+++ backend/report.py
@@ -0,0 +1,19 @@
+from random import randint, seed, sample
+import logging
+import repository as db
+
+log = logging.getLogger('report')
+seed(1)
+
+
+def _calculate_daily_reservations(hotel):
+    reservations = randint(0, hotel['rooms'])
+    log.info(f"Hotel {hotel['name']} had {reservations} reservations. "
+             f"Hotel is at {reservations/hotel['rooms']*100}% of its full capacity")
+
+
+if __name__ == "__main__":
+    hotels = db.find_hotels()
+
+    # Print reservation details for every hotel
+    [_calculate_daily_reservations(hotel) for hotel in sample(hotels, randint(1, len(hotels)))]
